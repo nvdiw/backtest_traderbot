a61f5c8557b55b80db4d8210d4fca0b424705f64
Navid <nouraeinavid@gmail.com>
2025-12-07 19:53:37 +0330

    now you can see profit with 1btc and last balance
    now you can see profit with 1btc and last balance but it is not too good for seeing information

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pandas as pd
 # start = 244944
 # end = 244994
 
+balance = 1000
+
 start = 244994
-end = 245102
+end = 245602
 
 is_order_open = False
 
@@ -52,14 +54,18 @@ def close_long(index):
         is_order_open = False
         close_prices = fetch_closing_prices(start, end)
         close_price = close_prices[index]
+        print("Close Long at price: ", close_price)
         return close_price
 
 def when_open_order():
+    global balance
+    
     close_prices = fetch_closing_prices(start, end)
     ma_9 = get_MA(9)
     ma_21 = get_MA(21)
     signals = []
     order = []
+    profits = []
     for i in range(len(close_prices)):
         if ma_9[i] is None or ma_21[i] is None:
             signals.append("No Signal")
@@ -69,19 +75,29 @@ def when_open_order():
             signals.append("Buy")
             open_order = open_long(i)
             if open_order is not None:
+                open_order_price = balance / open_order
                 order.append("open is " + str(open_order))
+                oo = open_order
 
         elif ma_9[i] < ma_21[i]:
             signals.append("Sell")
             close_order = close_long(i)
             if close_order is not None:
+                close_order_price = open_order_price * close_order
+                balance = close_order_price
                 order.append("close is " + str(close_order))
+                co = close_order
+                profit = co - oo
+                profits.append(round(profit, 2))
 
         else:
             signals.append("Hold")
 
-    print(order)
+    # print(order)
+    print("Final Balance: ", round(balance, 2))
+    print("Profits: ", profits)
+    print("Total Profit: ", sum(profits))
     return signals
 
-print(when_open_order())
-# when_open_order()
+# print(when_open_order())
+when_open_order()
