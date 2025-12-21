775ab058aa8aef3a7bea9e9bf00b79bcc1c23011
Navid <nouraeinavid@gmail.com>
2025-12-21 19:08:29 +0330

    big update open, long positions now in functions
    - big update open, long positions now are in functions
    - trademanager.py in going open, close positions processes
       we have class:  class TradeManager:
    - improve main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 from trade_csv_logger import TradeCSVLogger
 from indicators import Indicator
 from get_candle_index import get_candle_index
+from trademanager import TradeManager
 
 # 2025/01/01 first 15m candle of btc_15m_data.csv is: 244944 <--- start
 # the last last 15m candle of btc_15m_data.csv is:    278640 <--- end
@@ -17,12 +18,14 @@ from get_candle_index import get_candle_index
 # 2025/04/01 : 253584   # 2025/05/01 : 256464   # 2025/06/01 : 259440   ( bad )
 # 2025/07/01 : 262320   # 2025/08/01 : 265296   # 2025/09/01 : 268272   ( very bad )
 # 2025/10/01 : 271152   # 2025/11/01 : 274128   # 2025/12/01 : 277008   ( very good )
+
 # start = 244944
-# end = 277008
+# end = 278640
+# start = get_candle_index("2025-01-01")   ----> 244944
+# end = get_candle_index("2025-12-18")     ----> 278640
 
-
-start = get_candle_index("2025-01-01", "00:00")
-end = get_candle_index("2025-12-01", "00:00")
+start = get_candle_index("2025-01-01")
+end = get_candle_index("2025-12-18")
 
 current_position = None  # None | "long" | "short"
 
@@ -66,6 +69,8 @@ open_times = all_data["Open time"]
 close_times = all_data["Close time"]
 low_prices = all_data["Low"]
 high_prices = all_data["High"]
+
+
 # Calculate Trade Duration
 def trade_duration(open_time: str, close_time: str):
     # format: YYYY-MM-DD HH:MM:SS.microseconds
@@ -155,6 +160,7 @@ def execute_trading_logic():
         # print("len(ema_14):", len(ema_14))
         # print("len(ma_50):", len(ma_50))
 
+    trade_manager = TradeManager(csv_logger)
 
     for i in range(len(open_prices)):
 
@@ -293,250 +299,156 @@ def execute_trading_logic():
         # ===================== OPEN LONG =====================
         if ma_130[i] >= ma_200[i] and ema_14[i] > ma_50[i] and current_position is None:
             if ma_distance > ma_distance_threshold or last_candle_move > candle_move_threshold:
-                entry_price = open_prices[i]
-
-                balance_before_trade = balance
-                balance_before_trade_no_fee = balance_without_fee
-
-                # ---------- Margin ----------
-                if balance >= 50 / 100 * first_balance:
-                    margin = 50 / 100 * first_balance
-                else:
-                    margin = balance * trade_amount_percent
+                updates = trade_manager.open_long(
+                    i,
+                    open_prices,
+                    open_times,
+                    balance,
+                    balance_without_fee,
+                    first_balance,
+                    trade_amount_percent,
+                    total_balance,
+                    leverage)
                 
-                # ---------- Leverage ----------
-                if total_balance <= first_balance * 90 / 100:
-                    leverage = 3
-                else:
-                    leverage = 5
 
-                position_value = margin * leverage
-                position_size = position_value / entry_price
-
-                margin_no_fee = balance_without_fee * trade_amount_percent
-                position_value_no_fee = margin_no_fee * leverage
-                position_size_no_fee = position_value_no_fee / entry_price
-
-                # update balance after allocating margin
-                balance -= margin
-                balance_without_fee -= margin_no_fee
-
-                # update open time and current position
-                open_time_value = open_times[i]
-                current_position = "long"
-
-                print("Open LONG at price:", entry_price, "| Open Time:", open_time_value, "| leverage:", leverage)
+                entry_price = updates['entry_price']
+                balance = updates['balance']
+                balance_without_fee = updates['balance_without_fee']
+                balance_before_trade = updates['balance_before_trade']
+                balance_before_trade_no_fee = updates['balance_before_trade_no_fee']
+                margin = updates['margin']
+                leverage = updates['leverage']
+                position_size = updates['position_size']
+                margin_no_fee = updates['margin_no_fee']
+                position_size_no_fee = updates['position_size_no_fee']
+                open_time_value = updates['open_time_value']
+                current_position = updates['current_position']
 
         # ===================== CLOSE LONG =====================
         if current_position == "long":
             if (ema_14[i] < ma_50[i]) or (ma_130[i] < ma_200[i]):
 
-                close_price = open_prices[i]
-
-                # PnL
-                pnl = position_size * (close_price - entry_price)
-                pnl_no_fee = position_size_no_fee * (close_price - entry_price)
-
-                # Fee like Toobit
-                entry_fee = entry_price * position_size * fee_rate
-                exit_fee = close_price * position_size * fee_rate
-                total_fee = entry_fee + exit_fee
-
-                # Update balance
-                balance += margin + pnl - total_fee
-                balance_without_fee += margin_no_fee + pnl_no_fee
-
-                # profit after fee
-                profit = balance - balance_before_trade
-                profit_percent = profit * 100 / balance_before_trade
-                pnl_percent = (pnl / margin) * 100
-
-                deducting_fee_total += total_fee
-                profits_lst.append(profit)
-                count_closed_orders += 1
-
-                equity_curve.append(balance)
-                # ---- calculate max drawdown ----
-                peak = max(equity_curve)
-                drawdown = (balance - peak) / peak * 100
-                max_drawdown = min(max_drawdown, drawdown)
-
-                # ---- count wins and losses ----
-                if profit_percent > 0:
-                    total_wins += 1
-                else:
-                    total_losses += 1
-
-                # ---- count LONG trades ----
-                total_long += 1
-
-                # ---- COOLDOWN AFTER BIG PROFIT ----
-                pnl_percent_without_leverage = ((pnl / margin) * 100 ) / leverage
-                if pnl_percent_without_leverage >= 3 or pnl_percent_without_leverage <= -2:
-                    cooldown_until_index = i + cooldown_after_big_pnl
-                    print(f"🟡 Cooldown Activated (LONG) until candle index {cooldown_until_index}")
-
-                close_time_value = open_times[i]
-                days, hours, minutes = trade_duration(open_time_value, close_time_value)
-
-
-                print("Close LONG at price:", close_price, "| Close Time:", close_time_value, "| leverage:", leverage)
-                print("Balance:", round(balance_before_trade, 2), "→", round(balance, 2), "| Save Money:", round(save_money, 2))
-                print("Balance (no fee):",
-                    round(balance_before_trade_no_fee, 2), "→", round(balance_without_fee, 2))
-                print("pnl:", round(pnl, 2), "$ |", round(pnl_percent, 2), "% |" , "Amount:", round(margin), "$")
-                print("Profit:", round(profit, 2), "$ |", round(profit_percent, 2), "%")
-                print(f"Trade Duration: {days} days, {hours} hours, {minutes} minutes")
-                print("-" * 90)
-
-                csv_logger.log_trade(
-                    "LONG",
-                    open_time_value,
-                    close_time_value,
+                updates = trade_manager.close_long(
+                    i,
+                    open_prices,
+                    open_times,
                     entry_price,
-                    close_price,
-                    round(balance_before_trade, 2),
-                    round(balance, 2),
-                    round(margin , 2),
+                    position_size,
+                    position_size_no_fee,
+                    fee_rate,
+                    margin,
+                    margin_no_fee,
+                    balance,
+                    balance_without_fee,
+                    balance_before_trade,
+                    balance_before_trade_no_fee,
+                    deducting_fee_total,
+                    profits_lst,
+                    count_closed_orders,
+                    equity_curve,
+                    max_drawdown,
+                    total_wins,
+                    total_losses,
+                    total_long,
+                    cooldown_after_big_pnl,
                     leverage,
+                    cooldown_until_index,
+                    open_time_value,
+                    csv_logger,
                     trade_amount_percent,
-                    round(profit, 2),
-                    round(profit_percent, 2),
-                    round(pnl_percent, 2),
-                    round(total_fee, 4),
-                    days,
-                    hours,
-                    minutes,
-                    save_money
-                )
+                    save_money)
+                
 
-
-                current_position = None
+                balance = updates['balance']
+                balance_without_fee = updates['balance_without_fee']
+                deducting_fee_total = updates['deducting_fee_total']
+                profits_lst = updates['profits_lst']
+                count_closed_orders = updates['count_closed_orders']
+                equity_curve = updates['equity_curve']
+                max_drawdown = updates['max_drawdown']
+                total_wins = updates['total_wins']
+                total_losses = updates['total_losses']
+                total_long = updates['total_long']
+                cooldown_until_index = updates['cooldown_until_index']
+                current_position = updates['current_position']
 
 
         # ===================== OPEN SHORT =====================
         if ma_130[i] < ma_200[i] and ema_14[i] < ma_50[i] and current_position is None:
             if ma_distance > ma_distance_threshold or last_candle_move > candle_move_threshold:
 
-                entry_price = open_prices[i]
+                updates = trade_manager.open_short(
+                    i,
+                    open_prices,
+                    open_times,
+                    balance,
+                    balance_without_fee,
+                    first_balance,
+                    trade_amount_percent,
+                    total_balance,
+                    leverage)
+                
 
-                balance_before_trade = balance
-                balance_before_trade_no_fee = balance_without_fee
-
-                # ---------- LVR Margin ----------
-                if balance >= 50 / 100 * first_balance:
-                    margin = 50 / 100 * first_balance
-                else:
-                    margin = balance * trade_amount_percent
-
-                # ---------- Leverage ----------
-                if total_balance <= first_balance * 90 / 100:
-                    leverage = 3
-                else:
-                    leverage = 5
-
-                position_value = margin * leverage
-                position_size = position_value / entry_price
-
-                margin_no_fee = balance_without_fee * trade_amount_percent
-                position_value_no_fee = margin_no_fee * leverage
-                position_size_no_fee = position_value_no_fee / entry_price
-
-                # update balance after allocating margin
-                balance -= margin
-                balance_without_fee -= margin_no_fee
-
-                # update open time and current position
-                open_time_value = open_times[i]
-                current_position = "short"
-
-                print("Open SHORT at price:", entry_price, "| Open Time:", open_time_value, "| leverage:", leverage)
+                entry_price = updates['entry_price']
+                balance = updates['balance']
+                balance_without_fee = updates['balance_without_fee']
+                balance_before_trade = updates['balance_before_trade']
+                balance_before_trade_no_fee = updates['balance_before_trade_no_fee']
+                margin = updates['margin']
+                leverage = updates['leverage']
+                position_size = updates['position_size']
+                margin_no_fee = updates['margin_no_fee']
+                position_size_no_fee = updates['position_size_no_fee']
+                open_time_value = updates['open_time_value']
+                current_position = updates['current_position']
 
         # ===================== CLOSE SHORT =====================
         if current_position == "short":
             if (ema_14[i] > ma_50[i]) or (ma_130[i] >= ma_200[i]):
 
-                close_price = open_prices[i]
-
-                # PnL
-                pnl = position_size * (entry_price - close_price)
-                pnl_no_fee = position_size_no_fee * (entry_price - close_price)
-
-                # Fee like Toobit
-                entry_fee = entry_price * position_size * fee_rate
-                exit_fee = close_price * position_size * fee_rate
-                total_fee = entry_fee + exit_fee
-
-                # Update balance
-                balance += margin + pnl - total_fee
-                balance_without_fee += margin_no_fee + pnl_no_fee
-
-                # profit after fee
-                profit = balance - balance_before_trade
-                profit_percent = profit * 100 / balance_before_trade
-                pnl_percent = (pnl / margin) * 100
-
-                deducting_fee_total += total_fee
-                profits_lst.append(profit)
-                count_closed_orders += 1
-
-                equity_curve.append(balance)
-                # ---- calculate max drawdown ----
-                peak = max(equity_curve)
-                drawdown = (balance - peak) / peak * 100
-                max_drawdown = min(max_drawdown, drawdown)
-
-                # ---- count wins and losses ----
-                if profit_percent > 0:
-                    total_wins += 1
-                else:
-                    total_losses += 1
-
-                # ---- count shorts ----
-                total_short += 1
-
-                # ---- COOLDOWN AFTER BIG PROFIT ----
-                pnl_percent_without_leverage = ((pnl / margin) * 100) / leverage
-                if pnl_percent_without_leverage >= 3 or pnl_percent_without_leverage <= -2:
-                    cooldown_until_index = i + cooldown_after_big_pnl
-                    print(f"🟡 Cooldown Activated (SHORT) until candle index {cooldown_until_index}")
-
-
-                close_time_value = open_times[i]
-                days, hours, minutes = trade_duration(open_time_value, close_time_value)
-
-
-                print("Close SHORT at price:", close_price, "| Close Time:", close_time_value, "| leverage:", leverage)
-                print("Balance:", round(balance_before_trade, 2), "→", round(balance, 2), "| Save Money:", round(save_money, 2))
-                print("Balance (no fee):",
-                    round(balance_before_trade_no_fee, 2), "→", round(balance_without_fee, 2))
-                print("pnl:", round(pnl, 2), "$ |", round(pnl_percent, 2), "% |", "Amount:", round(margin), "$")
-                print("Profit:", round(profit, 2), "$ |", round(profit_percent, 2), "%")
-                print(f"Trade Duration: {days} days, {hours} hours, {minutes} minutes")
-                print("-" * 90)
-
-                csv_logger.log_trade(
-                    "SHORT",
-                    open_time_value,
-                    close_time_value,
+                updates = trade_manager.close_short(
+                    i,
+                    open_prices,
+                    open_times,
                     entry_price,
-                    close_price,
-                    round(balance_before_trade, 2),
-                    round(balance, 2),
-                    round(margin , 2),
+                    position_size,
+                    position_size_no_fee,
+                    fee_rate,
+                    margin,
+                    margin_no_fee,
+                    balance,
+                    balance_without_fee,
+                    balance_before_trade,
+                    balance_before_trade_no_fee,
+                    deducting_fee_total,
+                    profits_lst,
+                    count_closed_orders,
+                    equity_curve,
+                    max_drawdown,
+                    total_wins,
+                    total_losses,
+                    total_short,
+                    cooldown_after_big_pnl,
                     leverage,
+                    cooldown_until_index,
+                    open_time_value,
+                    csv_logger,
                     trade_amount_percent,
-                    round(profit, 2),
-                    round(profit_percent, 2),
-                    round(pnl_percent, 2),
-                    round(total_fee, 4),
-                    days,
-                    hours,
-                    minutes,
-                    save_money
-                )
+                    save_money)
+                    
 
-                current_position = None
+                balance = updates['balance']
+                balance_without_fee = updates['balance_without_fee']
+                deducting_fee_total = updates['deducting_fee_total']
+                profits_lst = updates['profits_lst']
+                count_closed_orders = updates['count_closed_orders']
+                equity_curve = updates['equity_curve']
+                max_drawdown = updates['max_drawdown']
+                total_wins = updates['total_wins']
+                total_losses = updates['total_losses']
+                total_short = updates['total_short']
+                cooldown_until_index = updates['cooldown_until_index']
+                current_position = updates['current_position']
 
     # ========== BACKTEST SUMMARY ==========
 
@@ -555,9 +467,9 @@ def execute_trading_logic():
     print("Closed Trades:", count_closed_orders, "( Longs:", total_long, "| Shorts:", total_short, ")")
     print("Total Wins:", total_wins)
     print("Total Losses:", total_losses)
-    print("Final Balance:", round(balance, 2))
-    print("Final Balance (No Fee):", round(balance_without_fee, 2))
-    print("Total Fees Paid:", round(deducting_fee_total, 2))
+    print("Final Balance:", round(balance, 2), "$")
+    print("Final Balance (No Fee):", round(balance_without_fee, 2), "$")
+    print("Total Fees Paid:", round(deducting_fee_total, 2), "$")
     print("Fee Compounding Impact:",
           round(balance_without_fee - balance - deducting_fee_total, 2), "$")
     print("Maximum Drawdown:", round(max_drawdown, 2), "%")
@@ -565,7 +477,7 @@ def execute_trading_logic():
     print("Win Rate:", round(win_rate, 2), "%")
     print("Total Profit:", round(sum(profits_lst), 2), "$")
     print("Total Profit Percent:", round(total_profit_percent, 2), "%")
-    # print("saved Money:", save_money, "$")
+    print("saved Money:", save_money, "$")
 
     csv_logger.save_csv(
     first_balance=first_balance,
@@ -587,3 +499,4 @@ execute_trading_logic()
 # print(open_prices[0])
 # print(open_times[0])
 # print(open_times[1])
+# print(start, end)
